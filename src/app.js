b1620c583c0fbdff23ec9516a3becd5ec9f6f05c
laflosh <l_florent_l@hotmail.fr>
2025-11-03 12:35:12 +0100

    add server file for axpress app

---
+++ src/app.js
@@ -0,0 +1,15 @@
+const express = require("express")
+
+const app = express()
+
+app.use((req, res, next) => {
+
+    res.setHeader("Access-Control-Allow-Origin","*");
+    res.setHeader("Access-Control-Allow-Headers", "Origin, X-requested-With, Content, Accept, Content-Type, Authorization");
+    res.setHeader("Access-Control-Allow-Methods","GET, POST, PUT, DELETE, PATCH, OPTION");
+    next();
+})
+
+app.use(express.json())
+
+module.exports = app
